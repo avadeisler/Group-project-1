843f8965176d295c955717738d5bf818f35d68ca
meistercraft <njmeister99@gmail.com>
2024-02-05 21:07:44 -0600

    trying to get things to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,5 @@
 // JavaScript
 
-// Get current location
-fetch('https://ipapi.co/json/')
-    .then(response => response.json())
-    .then(data => {
-        const city = data.city;
-        const country = data.country_name;
-        document.getElementById('currentLocation').innerText = `City: ${city}, Country: ${country}`;
-    })
-    .catch(error => {
-        console.error('Error fetching current location:', error);
-        document.getElementById('currentLocation').innerText = 'Error fetching current location';
-    });
-
-// Define your Meteomatics API credentials
-const username = 'home_asenciomorales_carlos';
-const password = 'KiyoL21L3u';
-
-// Construct the API request URL
-const apiUrl = 'https://api.meteomatics.com/2024-02-02T00:00:00ZP15D:P1D/moon_phase:idx/50,10/json';
-
-// Function to fetch moon phase information from Meteomatics API
-const fetchMoonPhase = () => {
-    fetch(apiUrl, {
-        headers: {
-            'Authorization': 'Basic ' + btoa(username + ':' + password)
-        }
-        
-    })
-    }
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch moon phase information');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const moonPhase = data.data[0].moon_phase.value;
-
-            // Display moon phase information
-            document.getElementById('moonPhase').innerHTML = `<p>Moon Phase: ${moonPhase}</p>`;
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error fetching moon phase data:', error);
-            document.getElementById('moonPhase').innerHTML = '<p>Error fetching moon phase information.</p>';
-        });
 // Display current date in the header using Day.js
 document.getElementById('currentDate').innerText = dayjs().format('dddd MM-DD-YY');
 
